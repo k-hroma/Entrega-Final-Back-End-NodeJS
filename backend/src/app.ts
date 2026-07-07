58635f8645b25704a9acb7f4b5b079f607fcb5cd
Croma <rociomendonca@gmail.com>
2026-07-07 11:17:13 -0300

    refactor middleware to use underscore for unused response parameter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
     console.log(`Datos recibidos: ${req.method} ${req.url}`);
     next();
 })
